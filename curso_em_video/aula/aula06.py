a7113cda939aee2f139df097c81adb4e54bf35a6
amosxrl <amosxrl@outlook.com>
2024-07-27 21:44:05 -0300

    mudanças na aula 06 do curso em video de python

--- curso_em_video/aula/aula06.py
+++ curso_em_video/aula/aula06.py
@@ -1,3 +1,4 @@
+
 inteiro = input('Valor a digitar: ')
 
 #"int"
@@ -5,6 +6,10 @@ inteiro = input('Valor a digitar: ')
 #"bool"
 #"str"
 #type()
-
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade? ')
+peso = input('Qual é o seu peso? ')
+print('Olá ', nome,' bem vindo ao python. ', idade, peso)
+print(' ')
 print(inteiro.isalnum())
 print(inteiro.isalpha())
