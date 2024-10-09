67e7ba4fd180de8f98212c3600d7e93e8e18e090
amosxrl <amosxrl@outlook.com>
2024-10-09 09:20:12 -0300

    reorganizar a aula06

--- curso_em_video/aula06.py
+++ curso_em_video/aula06.py
@@ -1,15 +1,14 @@
+nome = str(input('Qual é o seu nome? '))
+idade = int(input('Qual é a sua idade? '))
+peso = float(input('Qual é o seu peso? '))
+print('Olá', nome,'de', idade, 'anos de idade e peso', peso,'. Bem vindo ao python. ')
+print('-----------------------------------------------------------------------------')
 
 inteiro = input('Valor a digitar: ')
-
+print(inteiro.isalnum())
+print(inteiro.isalpha())
 #"int"
 #"float"
 #"bool"
 #"str"
 #type()
-nome = input('Qual é o seu nome? ')
-idade = input('Qual é a sua idade? ')
-peso = input('Qual é o seu peso? ')
-print('Olá ', nome,' bem vindo ao python. ', idade, peso)
-print(' ')
-print(inteiro.isalnum())
-print(inteiro.isalpha())
